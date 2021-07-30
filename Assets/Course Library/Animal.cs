da672b06b2fb7d47932ec530f22f970c757752c3
mulyarino <87961640+mulyarino@users.noreply.github.com>
2021-07-30 15:39:52 +0900

    ac
    ac

---
+++ Assets/Course Library/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class Animal : MonoBehaviour
+{
+
+    public GameObject canvas;
+    private float A_moveSpeed = 5f;
+    bool isGame;
+    public float moveSpeed
+
+    {
+        get { return A_moveSpeed; }
+        set { A_moveSpeed = value; }
+    }
+        
+
+    [SerializeField]
+    private GameObject[] animalPrefab;
+   
+
+    public void choice1()
+    {
+        Instantiate(animalPrefab[0],
+         animalPrefab[0].transform.position,
+         animalPrefab[0].transform.rotation);
+        canvas.gameObject.SetActive(false);
+    }
+    public void choice2()
+    {
+        Instantiate(animalPrefab[1], 
+        animalPrefab[1].transform.position,
+        animalPrefab[1].transform.rotation);
+        canvas.gameObject.SetActive(false);
+    }
+    public void choice3()
+    {
+        Instantiate(animalPrefab[2],
+        animalPrefab[2].transform.position,
+        animalPrefab[2].transform.rotation);
+        canvas.gameObject.SetActive(false);
+    }
+
+    public virtual void Move()
+    {
+        transform.Translate(Vector3.forward * moveSpeed  * Time.deltaTime);
+    }
+    
+        
+    
+}
---
+++ Assets/Course Library/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : Animal
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    public override void Move()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * 2 * Time.deltaTime);
+    }
+}
